fc93779f11b68425fae08108306bfd41dd01e395
Leonardo Quintania <leonardoquintania@gmail.com>
2022-02-23 08:22:26 -0300

    Adicionado desafio feito de upload CSV

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,75 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import { getCustomRepository, getRepository, In } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
 
+interface CSVTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const contactsReadStream = fs.createReadStream(filePath);
+    const parsers = csvParse({ from_line: 2 });
+
+    const parseCSV = contactsReadStream.pipe(parsers);
+
+    const transactions: Array<CSVTransaction> = [];
+    const categories: Array<string> = [];
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    const existCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existsCategoriesTitle = existCategories.map(
+      (category: Category) => category.title,
+    );
+
+    const addCategories = categories
+      .filter(category => !existsCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index); // -- Retira os duplicados
+
+    const newCategories = categoriesRepository.create(
+      addCategories.map(title => ({ title })),
+    );
+
+    await categoriesRepository.save(newCategories);
+    const finalCategories = [...newCategories, ...existCategories];
+    const createdTransactions = transactionRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+    await transactionRepository.save(createdTransactions);
+    await fs.promises.unlink(filePath); // -- Exclui arquivo após importação
+    return createdTransactions;
   }
 }
 
